1d02cb72f79d335d5e067ec4102af99c8e70b30d
QinshanSun <33112560+QinshanSun@users.noreply.github.com>
2021-04-13 22:03:40 +0800

    Create ActorController.java
    Create ActorController

---
+++ src/main/java/com/shan/tech/javlib/controller/ActorController.java
@@ -0,0 +1,33 @@
+package com.shan.tech.javlib.controller;
+
+import com.shan.tech.javlib.pojo.Actor;
+import com.shan.tech.javlib.service.ActorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/actor")
+public class ActorController {
+
+  private ActorService actorService;
+
+  @GetMapping
+  public Actor getActor(@RequestParam(name = "Id") Long id){
+    return  actorService.findById(id).orElseThrow();
+  }
+
+  @GetMapping
+  public Actor getActorByLabel(@RequestParam(name = "label") String label){
+    return  actorService.findByLabel(label).orElseThrow();
+  }
+
+  @PostMapping
+  public void insertActor(@RequestBody Actor actor){
+    actorService.insertActor(actor);
+  }
+
+  @Autowired
+  public void setActorService(ActorService actorService) {
+    this.actorService = actorService;
+  }
+}

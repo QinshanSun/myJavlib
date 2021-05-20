6ed6145cc2daa626053d476b38fc5a481133da3b
QinshanSun <33112560+QinshanSun@users.noreply.github.com>
2021-05-20 09:33:38 +0800

    Actor Kafka Consumer Support
    Actor Kafka Consumer Support

--- src/main/java/com/shan/tech/javlib/consts/KafkaConst.java
+++ src/main/java/com/shan/tech/javlib/consts/KafkaConst.java
@@ -4,6 +4,8 @@ public class KafkaConst {
 
   public final static String GENRE_TOPIC ="topic.jav.genre";
 
+  public final static String ACTOR_TOPIC ="topic.jav.actor";
+
   public final static int DEFAULT_PARTITION_NUM = 8;
 
   public final static short DEFAULT_REPLICATION_NUM = 1;
--- src/main/java/com/shan/tech/javlib/engine/KafkaConsumer.java
+++ src/main/java/com/shan/tech/javlib/engine/KafkaConsumer.java
@@ -3,7 +3,9 @@ package com.shan.tech.javlib.engine;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shan.tech.javlib.consts.KafkaConst;
+import com.shan.tech.javlib.pojo.Actor;
 import com.shan.tech.javlib.pojo.Genre;
+import com.shan.tech.javlib.service.ActorService;
 import com.shan.tech.javlib.service.GenreService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,13 +26,15 @@ public class KafkaConsumer {
 
   private GenreService genreService;
 
+  private ActorService actorService;
+
   @KafkaListener(topics = "users.topic", groupId = "group_id")
   public void consume(String message) {
     logger.info(String.format("#### -> Consumed message -> %s", message));
   }
 
   @KafkaListener(id = "genre", clientIdPrefix = "genre-batch",topics = {KafkaConst.GENRE_TOPIC}, containerFactory = "batchContainerFactory")
-  public void consume(@Payload List<String> genreList) {
+  public void consumeGenre(@Payload List<String> genreList) {
     logger.info("topic.quick.batch  receive : ");
     for (String s : genreList) {
       try {
@@ -43,8 +48,30 @@ public class KafkaConsumer {
   }
 
 
+  @KafkaListener(id = "actor", clientIdPrefix = "genre-batch",topics = {KafkaConst.ACTOR_TOPIC}, containerFactory = "batchContainerFactory")
+  public void consumeActor(@Payload List<String> actorList) {
+    logger.info("topic.quick.batch actor  receive : ");
+    for (String s : actorList) {
+      try {
+        Actor actor = objectMapper.readValue(s, Actor.class);
+        actor.setUpdatedDate(new Date());
+        actor.setCreatedDate(new Date());
+        int res = actorService.insertActor(actor);
+        logger.info("Actor: "+ actor + ",success: "+ res);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+
   @Autowired
   public void setGenreService(GenreService genreService) {
     this.genreService = genreService;
+  }
+
+  @Autowired
+  public void setActorService(ActorService actorService) {
+    this.actorService = actorService;
   }
 }

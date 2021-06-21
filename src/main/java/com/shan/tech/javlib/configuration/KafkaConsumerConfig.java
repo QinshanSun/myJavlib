85af1defc74cbccc941ed960c0e76f1580e74670
Qinshan Sun <33112560+QinshanSun@users.noreply.github.com>
2021-06-21 17:20:49 +0800

    Update Kafka Config Into Property file
    Update Kafka Config Into Property file

--- src/main/java/com/shan/tech/javlib/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/shan/tech/javlib/configuration/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.shan.tech.javlib.configuration;
 import com.shan.tech.javlib.consts.KafkaConst;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,10 +17,15 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String kafkaServers;
+
+
   @Bean
   public ConsumerFactory<String, String> defaultConsumerFactory() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -28,7 +34,7 @@ public class KafkaConsumerConfig {
 
   public Map<String, Object> batchConsumerProps() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
     props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
     props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
     props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, KafkaConst.MAX_POLL_RECORDS_CONFIG);
--- src/main/java/com/shan/tech/javlib/configuration/KafkaProducerConfig.java
+++ src/main/java/com/shan/tech/javlib/configuration/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.shan.tech.javlib.configuration;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,10 +14,14 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String kafkaServers;
+
   @Bean
   public ProducerFactory<String, String> producerFactory() {
     Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
     configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     return new DefaultKafkaProducerFactory<>(configProps);
